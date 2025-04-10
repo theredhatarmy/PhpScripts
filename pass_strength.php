e02faa45006598249ebfdbfd2f11ce3acc07d196
Kevin Kimanthi <46777371+theredhatarmy@users.noreply.github.com>
2025-04-10 18:24:11 +0300

    Update pass_strength.php

--- pass_strength.php
+++ pass_strength.php
@@ -2,7 +2,18 @@
 function checkPasswordStrength($password) {
     $strength = "Weak";
 
-    if (s
+    if (strlen($password) >= 8) {
+        $hasUpper  = preg_match('@[A-Z]@', $password);
+        $hasLower  = preg_match('@[a-z]@', $password);
+        $hasNumber = preg_match('@[0-9]@', $password);
+        $hasSymbol = preg_match('@[^\w]@', $password);
+
+        if ($hasUpper && $hasLower && $hasNumber && $hasSymbol) {
+            $strength = "Strong";
+        } elseif (($hasUpper || $hasLower) && $hasNumber) {
+            $strength = "Medium";
+        }
+    }
 
     return $strength;
 }
